b4c51f993181852075e1d018ff4d7f71f29c3ce5
THG Lab <thglab@dhcp-128-32-142-221.lips.berkeley.edu>
2013-11-20 15:52:02 -0800

    Working on proj at group meeting

--- ARSpackage/R/adapt_reject.r
+++ ARSpackage/R/adapt_reject.r
@@ -10,6 +10,9 @@
 #'    \item{\code{n}:}{Variable of class \code{"numeric"}, n, containing the number of points to sample}
 #'    \item{\code{h_x}:}{Function of class \code{"function"}, containing the log(f(x)) to sample from.}
 #'    \item{\code{h_prime}:}{Function of class \code{"function"}, containing the first derivative log(f(x)) to sample from.}
+#'    \item{\code{x}:}{Variable of class \code{"vector"}, containing points used to draw lines.}
+#'    \item{\code{z}:}{Variable of class \code{"vector"}, containing abscissae of upper bound function.}
+#'    \item{\code{output}:}{Variable of class \code{"vector"}, containing sampled points to return to user.}
 #'  }
 #'
 #' @note  1. Initialize
@@ -25,6 +28,25 @@
 #'   v) sample function from s(x) and uniform random number
 #'   vi) update steps
 #'   vii) error checking
+#'   
+#' Current questions:
+#'  1.  How do we draw a random number from sk(x)
+#'          i) calculate the area under each piece (Sk(x))
+#'          ii) divide by total area (Stot(x))
+#'          iii) weights <- Sk(x)/Stot(x)
+#'          iv) sample(1:k+1 with weights) -> select piece
+#'          v) rejection sample within the piece
+#'          
+#'          OR maybe a package?? spatstat with rpoint
+#'          
+#'  2.  How do we find initial points for x1 and x2?? All we know right now is that they need to encompass the max?  One needs pos deriv and one needs neg
+#'      method 1: gen random number and calculate h_prime
+#'      method 2: find 2 stdevs from mean, check if they fit criteria
+#'      
+#'  3. complete s(x)
+#'  4. need method to accept or reject and update ( both outputs and z )
+#'  
+#'          
 #' @name Cadapt_reject_sample 
 #' @rdname adapt_reject_sample
 #' @aliases Cadapt_reject_sample
@@ -77,9 +99,10 @@ setMethod("gen_x", signature = "Cadapt_reject_sample", function(object) {
 
 setMethod("initialize", "Cadapt_reject_sample", function(.Object, n , h_x, h_prime) {
      .Object@n <- n
-     .Object@output <- list()
+     .Object@output <- vector()
      .Object@h_x <- h_x
      .Object@h_prime <- h_prime
+     .Object@z <- vector()
      # determine x1 and x2, draw random numbers and then determine if their first derivatives are pos and neg.
      .Object@x <- .Object@gen_x( )
 })
@@ -92,7 +115,10 @@ setMethod("initialize", "Cadapt_reject_sample", function(.Object, n , h_x, h_pri
 #' @rdname ars-methods
 
 setMethod("show", signature = "Cadapt_reject_sample", function(object) {
-
+    print(" The number of samples taken:")
+    print(object@n)
+    print(" The samples taken:")
+    print(object@output)
 } )
 
 ######################################
@@ -113,8 +139,70 @@ setGeneric("error_check", function(object){standardGeneric("error_check")})
 
 
 setMethod("error_check", signature = "Cadapt_reject_sample", function(object) {
+    
     return( 1 )
 } )
+
+######################################
+######################################
+
+#' S(x) generic
+#' @param object An object
+
+setGeneric("s_x", function(object){standardGeneric("s_x")})
+
+
+######################################
+######################################
+
+#' Cadapt_reject_sample s(x)
+#' 
+#' Function to normalize the upper bounds of log(f(x))
+#' 
+#' @param object \code{\linkS4class{Cadapt_reject_sample}} object
+#' @rdname ars-methods
+#' 
+
+setMethod("s_x", signature = "Cadapt_reject_sample", function(object){
+    #Normalizing u_x
+    #The basic idea is just to find out the integration of u_x on the domain by adding the integration of each piecewise of u_x
+    #Seprate the domain into 3 parts: [x_0,z[1]],[z[1],z[k-1]],[z[k-1],x_a],x_0 and x_a are lower and upper bounds of the domain. If domain is R, then x_0=-inf, x_a=inf
+    #Use a forloop to calculate the integrations in [z[1],z[k-1]],each piece i is a line with slope h_prime(x[i]) and pass through the point (x[i],h(x[i])), and is from z[i-1] to z[i] 
+    k <- length(object@x)
+    for (i in 2:(k-1)){
+        piecewise_function<-function( x_prime, h_x_i, h_xprime_i, x_i ){
+            exp(h_xprime_i*(x_prime-x_i)+ h_x_i )
+        }
+    
+        hx_i <- object@h_x(object@x[i])
+        hx_p_i <- object@h_prime(object@x[i])
+        xi <- object@x[i]
+        
+        integration[i-1]<-integrate(piecewise_function,z[i-1],z[i], h_x_i=hx_i, h_xprime_i=hx_p_i, x_i=xi)[[1]]
+    }
+    #The function for u_x in [x_0,z[1]]
+    piecewise_function_1<-function(x_prime, h_x_1, h_xprime_1, x_1){
+        exp(object@h_prime(object@x[1])*(x_prime-object@x[1])+object@h_x(object@x[1]))
+    }
+    
+    #The function for u_x in [z[k],x_a]
+    piecewise_function_k<-function(x_prime, h_x_k, h_xprime_k, x_k){
+        exp(object@h_prime(object@x[k])*(x_prime-object@x[k])+object@h_x(object@x[k]))
+    }
+    
+    # Calculating constants for integration    
+    hx_1 <- object@h_prime(object@x[1])
+    hxprime_1 <- object@h_x(object@x[1])
+    x1 <- object@x[1]
+    hx_k <- object@h_x(object@x[k])
+    hxprime_k <- object@h_prime(object@x[k])
+    xk <- object@x[k]
+    
+    #Add up all 3 parts
+    normalized_factor<-integrate(piecewise_function_1,x_0,z[1], hx_1, hxprime_1, x1)+integrate(piecewise_function_k,z[k-1],x_a, hx_k, hxprime_k, xk)+sum(integration)
+    
+    weights <- 0
+})
 
 ######################################
 ######################################
@@ -134,7 +222,18 @@ setGeneric("upper", function(object){standardGeneric("upper")})
 
 
 setMethod("upper", signature = "Cadapt_reject_sample", function(object) {
-    return( 1 )  
+    #Figure out the vector of z
+    z<-vector()
+    # k is number of xs
+    for (i in 1:(k-1)){
+        z[i]<-(object@h_x(object@x[i+1])-object@h_x(object@x[i])-object@x[i+1]*object@h_prime(object@x[i+1])+object@x[i]*obejct@h_prime(object@x[i]))/(object@h_prime(object@x[i])-object@h_prime(object@x[i+1]))
+    }
+    #Calculate u of x star using the same method as we calculate l of x star
+    M<-as.integer(x_star>z)
+    J<-sum(M)
+    J_plus_one<-J+1
+    u_x_star<-object@h_x(object@x[J_plus_one])+(x_star-object@x[J_plus_one])*object@h_prime(object@x[J_plus_one])
+    return(u_x_star)
 } )
 
 ######################################
@@ -180,8 +279,39 @@ setGeneric("sample", function(object){standardGeneric("sample")})
 
 
 setMethod("sample", signature = "Cadapt_reject_sample", function(object) {
+    samples <- vector()
+    # Sample uniform random number
+    samples[1] <- runif( 1, min = 0, max = 1 )
+    
+    # Sample x_star from sk(x)
+    samples[2] <- object@sample_from_S(  )
+    
+    
     return( 1 )  
 } )
+
+######################################
+######################################
+
+#' Sample from S(x) generic
+#' @param object An object
+
+setGeneric("sample_from_S", function(object){standardGeneric("sample_from_S")})
+
+
+######################################
+######################################
+
+#' Cadapt_reject_sample sample_from_S
+#' @param object \code{\linkS4class{Cadapt_reject_sample}} object
+#' @rdname ars-methods
+
+setMethod("sample_from_S", signature = "Cadapt_reject_sample", function(object) {
+    
+    
+    return( 1 )  
+} )
+
 
 ######################################
 ######################################
@@ -222,14 +352,14 @@ a_r_s <- function( n_samples, log_fx, log_fx_prime, ... ){
     ars_class <- new( Cadapt_reject_sample, n=n_samples, h_x = log_fx, h_prime = log_fx_prime )
     
     # While we do not have enough n samples, continue to sample
-    while( length( .Object@output < n ) ){
-        .Object@u_x <- .Object@upper()
-        .Object@s_x <- .Object@s()
-        .Object@samples <- .Object@sample()
-        .Object@u_x_star <- .Object@upper()
-        .Object@u_x_star <- .Object@lower()
-        .Object@acc_rej()
-        .Object@update
+    while( length( ars_class@output < n ) ){
+        ars_class@u_x <- ars_class@upper()
+        ars_class@s_x <- ars_class@s()
+        ars_class@samples <- ars_class@sample()
+        ars_class@u_x_star <- ars_class@upper()
+        ars_class@u_x_star <- ars_class@lower()
+        ars_class@acc_rej()
+        ars_class@update
     }
     
     return( ars_class )
